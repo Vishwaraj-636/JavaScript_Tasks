7f5887d0126fa9a402203432af6231d6e7d8c3b1
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-01-12 10:34:07 +0530

    'like-buttton'

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-let main = document.querySelector('main')
-let btn = document.querySelector('button')
+let img = document.querySelector('img')
+let like =document.querySelector('#like')
 
-btn.addEventListener('click',function (){
-    let div = document.createElement('div');
-    
-    let x = Math.random()*90 
-    let y = Math.random()*90
-    let s = Math.random()*3
-    let r= Math.random()*360
-    let c1 =Math.floor(Math.random()*256)
-    let c2 =Math.floor(Math.random()*256)
-    let c3 =Math.floor(Math.random()*256)
-    div.style.width="50px"
-    div.style.height="50px"
-    div.style.position="absolute"
-    div.style.backgroundColor="rgb("+c1+","+c2+","+c3+")"
-    div.style.left=x+"%"
-    div.style.top=y+"%"
-    div.style.rotate=r+"deg"
-    
-    main.appendChild(div)
-})
+img.addEventListener('dblclick',function (){
+    like.style.opacity ="1"
+    like.style.transform="translate(-50%,-50%) scale(5) "
+    setTimeout(function (){
+        like.style.transform="translate(-50%,-1100%) scale(5) rotate(60deg)"
+    },400)
+    setTimeout(function (){
+        like.style.opacity ="0"
+    },600)
+    setTimeout(function (){
+        like.style.transform="translate(-50%,-50%) scale(0) "
+    },800)
+}
+)
